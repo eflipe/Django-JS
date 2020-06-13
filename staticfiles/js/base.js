92d536874d1c57fbf59572aeb338dcc9eaf8c327
eflipe <felipecabaleiro@gmail.com>
2020-06-13 13:55:15 -0300

    + staticfiles

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -1,30 +1,5 @@
 console.log("JS aquí!");
-$("#menu-close").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
-
-// <a id="menu-toggle" href="#" class="btn btn-default btn-sm toggle"><i class="glyphicon glyphicon-flash"></i></a>
-// <div id="sidebar-wrapper">
-//   <ul class="sidebar-nav">
-//     <a id="menu-close" href="#" class="btn btn-default btn-lg pull-right toggle"><i class="glyphicon glyphicon-remove"></i></a>
-//     <li class="sidebar-brand">
-//       <a href="{% url 'pics_list' %}">Galeria</a>
-//     </li>
-//     <li>
-//       <a href="{% url 'pics_list' %}">Inicio</a>
-//     </li>
-//     <li>
-//       <a href="{% url 'authors_list' %}">Artistas</a>
-//     </li>
-//     <li>
-//       <a href="{% url 'account_login' %}">Admin</a>
-//     </li>
-//   </ul>
-// </div>
-
-//d-block mb-4 h-100
+// $( '#navbar' ).on( 'click', function () {
+// 	$( '#navbar' ).find( 'li.active' ).removeClass( 'active' );
+// 	$( this ).parent( 'li' ).addClass( 'active' );
+// });
