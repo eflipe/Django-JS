e89a268d730f3c1f37c5380997afcc1a38cc2cc9
eflipe <felipecabaleiro@gmail.com>
2020-06-14 15:03:37 -0300

    ++Deploy

--- galeria_project/settings.py
+++ galeria_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['cryptic-waters-52358.herokuapp.com', '192.168.99.100', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['heyheymycode.herokuapp.com','cryptic-waters-52358.herokuapp.com', '192.168.99.100', '127.0.0.1', 'localhost']
 
 
 # Application definition
