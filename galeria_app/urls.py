8cc4f4e1c6c9b348bacb89748e8d351388ce999a
eflipe <felipecabaleiro@gmail.com>
2020-06-14 01:31:10 -0300

    Ajustes CSS

--- galeria_app/urls.py
+++ galeria_app/urls.py
@@ -4,6 +4,7 @@ from .views import PicsListView,PicsListViewJS, AuthorsListView, AuthorsDetailVi
 
 urlpatterns = [
     path('', PicsListView.as_view(), name="pics_list"),
+    path('v1/', PicsListView.as_view(), name="pics_v1"),
     path('artistas/', AuthorsListView.as_view(), name="authors_list"),
     path('artistas/<slug:slug>', AuthorsDetailView.as_view(), name="authors_detail"),
     path('search/', SearchResultsView.as_view(), name="search_results"),
--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,21 @@
 console.log("JS aquí!");
-// $( '#navbar' ).on( 'click', function () {
-// 	$( '#navbar' ).find( 'li.active' ).removeClass( 'active' );
-// 	$( this ).parent( 'li' ).addClass( 'active' );
+// const navItem = document.querySelectorAll(".nav-item");
+
+// function toggleClass(event) {
+//   const tgt = event.currentTarget;
+//   console.log(tgt);
+//   tgt.classList.toggle("active");
+// }
+//
+// navItem.forEach((item) => {
+//   item.addEventListener("click", toggleClass);
 // });
+
+// for (let i = 0; i < navItem.length; i++) {
+//   console.log(navItem[i]);
+//   navItem[i].addEventListener("click", toggle);
+// }
+//
+// function toggle() {
+//   this.classList.toggle("active");
+// }
