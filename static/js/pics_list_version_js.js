c65c383133df23391056991533eb5c7ed1f70e09
eflipe <felipecabaleiro@gmail.com>
2020-06-23 16:32:01 -0300

    Nuevo JS

--- static/js/pics_list_version_js.js
+++ static/js/pics_list_version_js.js
@@ -1,98 +1,108 @@
 console.log("JS v2");
 
 function Galeria(galeria) {
-  const images = Array.from(galeria.querySelectorAll("img"));
 
-  const modal = document.querySelector(".modal");
-  const prevB = document.querySelector(".prev");
-  const nextB = document.querySelector(".next");
-  let currentImg;
+    this.galeria = galeria;
 
-  // --- funciones ----
+    this.images = Array.from(galeria.querySelectorAll("img"));
+    this.modal = document.querySelector(".modal");
+    this.prevB = this.modal.querySelector(".prev");
+    this.nextB = this.modal.querySelector(".next");
 
-  //fc: open foto
-  function openModal() {
-    if (modal.matches(".open")) {
-      return;
-    }
-    modal.classList.add("open");
-
-    window.addEventListener("keyup", keyUp);
-    nextB.addEventListener("click", nextImg);
-    prevB.addEventListener("click", prevImg);
-  }
-
-  //fc: close
-  function closeModal() {
-    modal.classList.remove("open");
-
-    window.removeEventListener("keyup", keyUp);
-    nextB.removeEventListener("click", nextImg);
-    prevB.removeEventListener("click", prevImg);
-  }
-
-  function clickCloseModal(event) {
-    if (event.target === event.currentTarget) {
-      closeModal();
-    }
-  }
-
-  //fc: showImage
-  function showImage(event) {
-    if (!event) {
-      console.info("No img");
-      return;
-    }
-    console.log(event);
-    modal.querySelector("img").src = event.src;
-    modal.querySelector("h2").textContent = event.title;
-    modal.querySelector("figure p").textContent = event.dataset.description;
-    modal.querySelector("figure a").href = event.dataset.link;
-    currentImg = event;
-    openModal();
-  }
-
-  //fc: click image
-  function imageClik(event) {
-    showImage(event.currentTarget);
-  }
-
-  //fc: key press
-
-  function keyUp(event) {
-    if (event.key === "Escape") return closeModal();
-    if (event.key === "ArrowRight") return nextImg();
-    if (event.key === "ArrowLeft") return prevImg();
-  }
-
-  // fc: next img
-
-  function nextImg() {
-    showImage(currentImg.nextElementSibling || galeria.firstElementChild);
-  }
-
-  // fc: prev img
-
-  function prevImg() {
-    showImage(currentImg.previousElementSibling || galeria.lastElementChild);
-  }
+  // bind methods to the instance
+  this.nextImg = this.nextImg.bind(this)
+  this.prevImg = this.prevImg.bind(this)
+  this.keyUp = this.keyUp.bind(this)
+  this.clickCloseModal = this.clickCloseModal.bind(this)
 
   // --- EVENTS ----
 
-  images.forEach((image) => {
-    image.addEventListener("click", (event) => showImage(event.currentTarget));
+  this.images.forEach((image) => {
+    image.addEventListener("click", (event) => this.showImage(event.currentTarget));
   });
 
-  images.forEach((image) => {
+  this.images.forEach((image) => {
     image.addEventListener("keyup", (event) => {
       if (event.key === "Enter") {
         console.log("holis");
-        showImage(event.currentTarget);
+        this.showImage(event.currentTarget);
       }
     });
   });
 
-  modal.addEventListener("click", clickCloseModal);
+  this.modal.addEventListener("click", this.clickCloseModal);
 }
 
-const galeria_1 = Galeria(document.querySelector(".galeria"));
+// --- funciones ----
+
+//fc: open foto
+Galeria.prototype.openModal = function () {
+  if (this.modal.matches(".open")) {
+    return;
+  }
+  this.modal.classList.add("open");
+
+  window.addEventListener("keyup", this.keyUp);
+  this.nextB.addEventListener("click", this.nextImg);
+  this.prevB.addEventListener("click", this.prevImg);
+}
+
+//fc: close
+Galeria.prototype.closeModal = function() {
+  this.modal.classList.remove("open");
+
+  window.removeEventListener("keyup", this.keyUp);
+  this.nextB.removeEventListener("click", this.nextImg);
+  this.prevB.removeEventListener("click", this.prevImg);
+}
+
+Galeria.prototype.clickCloseModal = function(event) {
+  if (event.target === event.currentTarget) {
+    this.closeModal();
+  }
+}
+
+//fc: showImage
+Galeria.prototype.showImage = function(event) {
+  if (!event) {
+    console.info("No img");
+    return;
+  }
+
+  this.modal.querySelector("img").src = event.src;
+  this.modal.querySelector("h2").textContent = event.title;
+  this.modal.querySelector("figure p").textContent = event.dataset.description;
+  this.modal.querySelector("figure a").href = event.dataset.link;
+  this.currentImg = event;
+  this.openModal();
+}
+
+//fc: click image
+Galeria.prototype.imageClik = function(event) {
+  this.showImage(event.currentTarget);
+}
+
+//fc: key press
+
+Galeria.prototype.keyUp = function(event) {
+  if (event.key === "Escape") return this.closeModal();
+  if (event.key === "ArrowRight") return this.nextImg();
+  if (event.key === "ArrowLeft") return this.prevImg();
+}
+
+// fc: next img
+
+Galeria.prototype.nextImg  = function() {
+  this.showImage(this.currentImg.nextElementSibling || this.galeria.firstElementChild);
+}
+
+// fc: prev img
+
+Galeria.prototype.prevImg = function() {
+  this.showImage(this.currentImg.previousElementSibling || this.galeria.lastElementChild);
+}
+
+
+
+
+const galeria_1 = new Galeria(document.querySelector(".galeria"));
